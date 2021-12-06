9e5084bc881378329654d39ba1bc8b24173c1e54
Faker1218 <95613169+Faker1218@users.noreply.github.com>
2021-12-06 17:41:22 +0800

    Add files via upload

---
+++ Enemy/enemies.py
@@ -0,0 +1,13 @@
+import pygame
+import random
+
+class Enemy():
+    def __init__(self,img="./Enemy/enemy.png"):
+        self.enemyImg=pygame.image.load(img)
+        self.x=random.randint(200,600)
+        self.y=random.randint(50,250)
+        self.step=random.randint(2,6)
+
+    def reset(self):
+        self.x=random.randint(200,600)
+        self.y=random.randint(50,250)
