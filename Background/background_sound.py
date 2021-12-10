e70594d93775b0cdd6166c1d9bd7ca6a4453cfbc
CaptainRui1000 <94994001+CaptainRui1000@users.noreply.github.com>
2021-12-11 01:28:15 +0800

    fix background bug

--- Background/background_sound.py
+++ Background/background_sound.py
@@ -2,16 +2,14 @@ import pygame
 
 
 class Background(pygame.sprite.Sprite):
-    def __init__(self,speed=1,img='./Background/bg.png'):
+    def __init__(self, stage=0, speed=1, img='./Background/bg.png'):
         super().__init__()
-        self.image=pygame.image.load(img)
-        self.speed=speed
+        self.stage = stage
+        self.image = pygame.image.load(img)
+        self.speed = speed
         self.rect = self.image.get_rect()
 
     def update(self):
-        self.rect.y+=self.speed
-        if self.rect.y>=self.image.get_height():
-            self.rect.y=-self.image.get_height()
-        
-
-        
+        self.rect.y += self.speed
+        if self.rect.y >= self.image.get_height():
+            self.rect.y = -self.image.get_height()
